2f484bd8927f303079b9a5b9f6dcab35d813a9e4
Arjen Bos <arjen.bos@atabix.nl>
2016-11-16 11:59:27 +0100

    updates

--- PHP7/recipes/default.rb
+++ PHP7/recipes/default.rb
@@ -1,9 +1,13 @@
-script "Run a script" do
+script "Install PHP7.0" do
   interpreter "bash"
   user 'root'
   code <<-EOH
       LC_ALL=en_US.UTF-8 add-apt-repository -y ppa:ondrej/php
       apt-get update
-      apt-get install -y php7.0-fpm php7.0-common php7.0-mysql
+      apt-get install -y php7.0-fpm php7.0-common php7.0-mysql php7.0-mbstring php7.0-mcrypt php7.0-xml curl
+      curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+      chmod +x /usr/local/bin/composer
+      sed -i "/listen = .*/c\listen = [::]:9000" /etc/php/7.0/fpm/pool.d/www.conf
+      service php7.0-fpm restart
   EOH
 end
