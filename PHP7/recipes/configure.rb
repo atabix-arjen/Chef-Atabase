54ced347e8e5452ef0d59423abae5314277d067e
Arjen Bos <arjen.bos@atabix.nl>
2016-11-16 17:46:31 +0100

    OpsWorks

---
+++ PHP7/recipes/configure.rb
@@ -0,0 +1,20 @@
+node[:deploy].each do |application, deploy|
+
+  # write out opsworks.php
+  template "#{deploy[:deploy_to]}/shared/config/opsworks.php" do
+    cookbook 'php'
+    source 'opsworks.php.erb'
+    mode '0660'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+      :database => deploy[:database],
+      :memcached => deploy[:memcached],
+      :layers => node[:opsworks][:layers],
+      :stack_name => node[:opsworks][:stack][:name]
+    )
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/config")
+    end
+  end
+end
--- PHP7/recipes/default.rb
+++ PHP7/recipes/default.rb
@@ -4,7 +4,7 @@ script "Install PHP7.0" do
   code <<-EOH
       LC_ALL=en_US.UTF-8 add-apt-repository -y ppa:ondrej/php
       apt-get update
-      apt-get install -y php7.0-fpm php7.0-common php7.0-mysql php7.0-mbstring php7.0-mcrypt php7.0-xml php7.0-memcached php7.0-zip curl
+      apt-get install -y php7.0-fpm php7.0-common php7.0-mysql php7.0-mbstring php7.0-mcrypt php7.0-xml php7.0-memcached php7.0-zip curl php7.0-curl
       curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
       chmod +x /usr/local/bin/composer
       sed -i "/listen = .*/c\listen = [::]:9000" /etc/php/7.0/fpm/pool.d/www.conf
