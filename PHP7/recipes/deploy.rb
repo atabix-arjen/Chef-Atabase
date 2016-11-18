cb01df071f4c683e9e1044d880f38a6de87b0f6a
Arjen Bos <arjen.bos@atabix.nl>
2016-11-18 17:43:25 +0100

    Update deploy.rb

--- PHP7/recipes/deploy.rb
+++ PHP7/recipes/deploy.rb
@@ -5,18 +5,27 @@
 #      (cd /var/www/ && composer create-project --prefer-dist laravel/laravel production && chown www-data:www-data -R production)
 #  EOH
 #end
+node[:deploy].each do |application, deploy|
+    cookbook_file '/etc/nginx/sites-enabled/production' do
+        source 'laravel'
+        mode 0644
+        owner 'root'
+        group 'root'
+    end
 
-cookbook_file '/etc/nginx/sites-enabled/production' do
-    source 'laravel'
-    mode 0644
-    owner 'root'
-    group 'root'
-end
+    script "CHOWN " do
+      interpreter "bash"
+      user 'root'
+      code <<-EOH
+          (cd #{deploy[:deploy_to]}/releases/current && chown www-data:www-data -R *)
+      EOH
+    end
 
-script "Restart NGINX" do
-  interpreter "bash"
-  user 'root'
-  code <<-EOH
-      service nginx restart
-  EOH
+    script "Restart NGINX" do
+      interpreter "bash"
+      user 'root'
+      code <<-EOH
+          service nginx restart
+      EOH
+    end
 end
