72e5d21990fc64c307efc39badd08c428fc69da4
Arjen Bos <arjen.bos@atabix.nl>
2016-11-18 13:41:58 +0100

    comment out laravel

--- PHP7/recipes/deploy.rb
+++ PHP7/recipes/deploy.rb
@@ -1,10 +1,10 @@
-script "Install Laravel" do
-  interpreter "bash"
-  user 'root'
-  code <<-EOH
-      (cd /var/www/ && composer create-project --prefer-dist laravel/laravel production && chown www-data:www-data -R production)
-  EOH
-end
+#script "Install Laravel" do
+#  interpreter "bash"
+#  user 'root'
+#  code <<-EOH
+#      (cd /var/www/ && composer create-project --prefer-dist laravel/laravel production && chown www-data:www-data -R production)
+#  EOH
+#end
 
 cookbook_file '/etc/nginx/sites-enabled/production' do
     source 'laravel'
